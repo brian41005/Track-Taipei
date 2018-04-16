3f87eae2e766b92b2a031e315924f84af16d76bf
Z-Xuan Hong <gay88358@yahoo.com.tw>
2018-04-16 21:20:45 +0800

    change google map address

--- index.js
+++ index.js
@@ -25,4 +25,3 @@ homeController(app, player);
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
--- js/run.js
+++ js/run.js
@@ -74,29 +74,31 @@ $(document).ready(function() {
         left2: '40',
         top3: '55',
         left3: '45',
-    }];        
-    
+    }];
+
     positions = [pointsPosition, tabletPosition];
 
-    // add br
 
     data = [{
+        // still need to change IFrame URL
+        // titleIFrame, trackIFrame1, trackIFrame2, trackIFrame3
         mainPhoto: '/images/大稻埕.svg',
         titleIFrame: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/423561195&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
         titleIntro: '大稻埕是臺北市大同區西南部的一個傳統地域名稱，18世紀末，因淡水港的開放後成為商貿繁榮、人文薈萃之地。大稻埕擁有華麗的巴洛克式建築、傳統的閩南平房、明亮的紅磚洋樓，不論是古蹟建築、傳統民俗、茶行、布行、中藥材行、在地美食等，到處都有舊城的歷史軌跡。而如今因著擁有深厚歷史傳承的百年老舖與創意街區，打造出大稻埕新生活美學。',
-        googleMapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.468451959292!2d121.5164839646644!3d25.05210690266453!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a96cf2fee5ad%3A0x19d4a2f503dc959c!2zMTAz5Y-w5YyX5biC5aSn5ZCM5Y2A5aSq5Y6f6LevOTflt7c!5e0!3m2!1szh-TW!2stw!4v1518844031617',
+        googleMapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.279882060915!2d121.50741011509105!3d25.058501243462604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a915c7d73db9%3A0xb031aa4cebcf6cea!2zMTAz5Y-w5YyX5biC5aSn5ZCM5Y2A6L-q5YyW6KGX5LiA5q61!5e0!3m2!1szh-TW!2stw!4v1523884654831',
         trackIntro: '時光荏苒<br>無船的碼頭河川依然湧動<br>街道裡乾貨與藥材香氣依舊<br>穿梭於不同時間與空間<br>我聽到新舊融合譜出的節奏',
-        trackMapImg: 'images/大稻埕_track.svg', 
+        trackMapImg: 'images/大稻埕_track.svg',
         trackIFrame1: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/423561183&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
         trackIFrame2: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/423561174&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
         trackIFrame3: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/423561171&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     }, {
+        // <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.4380170403642!2d121.51378491509108!3d25.053139043677866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a96c997b2e3f%3A0x6cab14efb813bd7!2zMTAz5Y-w5YyX5biC5aSn5ZCM5Y2A5aSq5Y6f6Lev!5e0!3m2!1szh-TW!2stw!4v1523884576532" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
         mainPhoto: '/images/main1.svg',
         titleIFrame: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/423561195&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
         titleIntro: '位於昔日台北火車站後站出口的太原路周邊，過去是北部地區各類五金雜貨、文具、時裝、飾品的批發市場，全盛時期曾高達數十家。而現在太原路上則充斥大量的塑膠製品，各式規格塑膠水桶、塑膠容器、塑膠布、塑膠捲，各式製品琳琅滿目，應有盡有。',
-        googleMapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.468451959292!2d121.5164839646644!3d25.05210690266453!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a96cf2fee5ad%3A0x19d4a2f503dc959c!2zMTAz5Y-w5YyX5biC5aSn5ZCM5Y2A5aSq5Y6f6LevOTflt7c!5e0!3m2!1szh-TW!2stw!4v1518844031617',
+        googleMapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.4380170403642!2d121.51378491509108!3d25.053139043677866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a96c997b2e3f%3A0x6cab14efb813bd7!2zMTAz5Y-w5YyX5biC5aSn5ZCM5Y2A5aSq5Y6f6Lev!5e0!3m2!1szh-TW!2stw!4v1523884576532',
         trackIntro: '玻璃瓶罐互相碰撞<br>焊接擦出零星的火花<br>俯拾皆是生活的碎片',
-        trackMapImg: 'images/0319 tablet  -17.svg', 
+        trackMapImg: 'images/0319 tablet  -17.svg',
         trackIFrame1: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/423561183&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
         trackIFrame2: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/423561174&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
         trackIFrame3: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/423561171&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
@@ -104,39 +106,38 @@ $(document).ready(function() {
         mainPhoto: '/images/東門市場.svg',
         titleIFrame: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/423564201&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
         titleIntro: '歷經清治時期、日治時期、戰後時期，興建於民國十七年的東門市場是個歷史悠久的傳統市場，充滿了許多老饕客的共同記憶和現代人的新穎口味。無論是彈牙的魚丸、酥脆的餡餅、鮮甜的米粉湯，蜿蜒的小巷弄內隱藏著道地的庶民小吃。在各地眷村紛紛拆遷的現代，東門市場的美食成了另類的臺北史紀錄。',
-        googleMapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.986160014005!2d121.52451861541354!3d25.03454374442266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a982bcb19f97%3A0x431fc1c5030a686!2zMTAw5Y-w5YyX5biC5Lit5q2j5Y2A5L-h576p6Lev5LqM5q61ODHomZ8!5e0!3m2!1szh-TW!2stw!4v1519643520638',
+        googleMapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.9861600139798!2d121.5245186150908!3d25.03454374442349!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a982bcb19f97%3A0x431fc1c5030a686!2zMTAw5Y-w5YyX5biC5Lit5q2j5Y2A5L-h576p6Lev5LqM5q61ODHomZ8!5e0!3m2!1szh-TW!2stw!4v1523884421783',
         trackIntro: '菜車輪似乎偏了，轆轆聲中時不時混雜嘰呀的聲音<br>緩緩地進入一片叫賣聲、殺價聲、談笑聲裡<br>新的一天又要開始了',
-        trackMapImg: 'images/東門市場_track.svg', 
+        trackMapImg: 'images/東門市場_track.svg',
         trackIFrame1: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/423564177&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
         trackIFrame2: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/423564165&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
         trackIFrame3: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/423564150&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     }, {
-        // <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src=""></iframe>
-        // <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src=""></iframe>
-        // <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src=""></iframe> 
         mainPhoto: '/images/龍山.svg',
         titleIFrame: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/427225557&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
         titleIntro: '艋岬，又名萬華，是台北市開發最早的地方。而「龍山寺」是當地居 民信仰、活動、集會和指揮的中心，保佑著過去渡海來台的先民平安順利，富有早期居民打拚、奮鬥的文化歷史。龍山寺平日香火鼎盛，其獨特的建築特色極具藝術價值，附近的懷舊氣氛更讓龍山寺成為台北著名的觀光景點。',
-        googleMapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.468451959292!2d121.5164839646644!3d25.05210690266453!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a96cf2fee5ad%3A0x19d4a2f503dc959c!2zMTAz5Y-w5YyX5biC5aSn5ZCM5Y2A5aSq5Y6f6LevOTflt7c!5e0!3m2!1szh-TW!2stw!4v1518844031617',
+        googleMapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.919664331485!2d121.49774191509087!3d25.036800244332902!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a9a8d871dd61%3A0x458407a147737730!2zMTA45Y-w5YyX5biC6JCs6I-v5Y2A5buj5bee6KGXMjEx6Jmf!5e0!3m2!1szh-TW!2stw!4v1523884728447',
         trackIntro: '轉角後傳來陣陣誦經<br>定時敲起的木魚、銅鈴<br>耳邊的呢喃是心中的渴求<br>亦是人們共同懷有的虔誠與執念',
-        trackMapImg: 'images/龍山_track.svg', 
+        trackMapImg: 'images/龍山_track.svg',
         trackIFrame1: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/427225521&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
         trackIFrame2: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/427225494&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
         trackIFrame3: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/427225347&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     }, {
+        // still need to change IFrame URL
+        // titleIFrame, trackIFrame1, trackIFrame2, trackIFrame3
         mainPhoto: '/images/羅文化.svg',
         titleIFrame: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/423564201&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
         titleIntro: '溫羅汀是在台灣大學與師範大學中間，由溫州街、羅斯福路、汀州街一帶交織而成的區域。在這人文空間最密集的區域，大大小小的獨立書店、咖啡廳、live house聚集於此，成為可觀的人文風景。',
-        googleMapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.468451959292!2d121.5164839646644!3d25.05210690266453!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a96cf2fee5ad%3A0x19d4a2f503dc959c!2zMTAz5Y-w5YyX5biC5aSn5ZCM5Y2A5aSq5Y6f6LevOTflt7c!5e0!3m2!1szh-TW!2stw!4v1518844031617',
+        googleMapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3615.3377409671516!2d121.53104391509052!3d25.02260984490175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a9889a450a73%3A0xaffa2ef96fa39ee5!2zMTA25Y-w5YyX5biC5aSn5a6J5Y2A5rqr5bee6KGX!5e0!3m2!1szh-TW!2stw!4v1523884692976',
         trackIntro: '翻動書頁，細細交談<br>字句在空氣中飄盪<br>安靜在這裡似乎成為一種聲音',
-        trackMapImg: 'images/羅文化_track.svg', 
+        trackMapImg: 'images/羅文化_track.svg',
         trackIFrame1: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/423561183&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
         trackIFrame2: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/423561174&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
         trackIFrame3: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/423561171&color=%232d0d15&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
-    }];  
+    }];
 
 
-    initializePageStatus(); 
+    initializePageStatus();
 
     function initializePageStatus() {
         // active headerMusic's bottom border
@@ -148,9 +149,9 @@ $(document).ready(function() {
         // dynamic setting the size of all points
         initializePlaceSize();
         // dynamic setting the photo of the device
-        
-        
-        
+
+
+
         setMapImgByWidthOfDevice();
     }
 
@@ -203,7 +204,7 @@ $(document).ready(function() {
         var windowWidth = $(window).width();
         return windowWidth;
     }
-    
+
     function replaceIntroContentByIndex(index) {
         model = data[index];
         var title = ['看看是誰組成大稻埕吧', '看看是誰組成太原路吧', '看看是誰組成東門市場吧', '看看是誰組成龍山寺吧', '看看是誰組成溫羅玎文化圈吧！'];
@@ -212,7 +213,7 @@ $(document).ready(function() {
 
         console.log(getDeviceWidth());
         console.log(positions[0]);
-        
+
         posModel = positions[Math.floor(getDeviceWidth() / 760)][index];
         //posModel = pointsPosition[index];
         replaceTrackContent(model, posModel);
@@ -239,7 +240,7 @@ $(document).ready(function() {
         $('#track-iframe1').attr('src', model.trackIFrame1);
         $('#track-iframe2').attr('src', model.trackIFrame2);
         $('#track-iframe3').attr('src', model.trackIFrame3);
-        
+
         $('.point1').css('top', posModel.top1 + '%');
         $('.point1').css('left', posModel.left1 + '%');
         $('.point2').css('top', posModel.top2 + '%');
@@ -255,7 +256,7 @@ $(document).ready(function() {
     }
 
 
-    
+
     function startSpinning() {
         $('.spinner-container').show();
     }
